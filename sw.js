9d536a11531d57ec544a5672e43b8f274c80f766
losinaive-dot <losinaive@gmail.com>
2026-08-08 19:45:41 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+// MAS 영업 파이프라인 - 서비스 워커
+// 이 파일이 있어야 크롬/엣지/삼성인터넷 등에서 "설치" 메뉴가 나타납니다.
+const CACHE_NAME = "mas-crm-v1";
+const ASSETS = [
+  "./mas_crm.html",
+  "./manifest.json",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request)
+        .then((res) => {
+          if (res && res.status === 200 && event.request.method === "GET") {
+            const clone = res.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          }
+          return res;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
